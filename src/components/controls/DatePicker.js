1c0190f8e056a0bf51db2ae6dc5c408d97fd2cf7
Earnest Odhiambo Achayo <earnytechlive@gmail.com>
2023-11-07 12:41:38 +0300

    added date picker functionalities

--- src/components/controls/DatePicker.js
+++ src/components/controls/DatePicker.js
@@ -1,21 +1,29 @@
-import React from "react";
-import { MuiPickersUtilsProvider } from "@material-ui/pickers";
-import DateFnsUtils from "@date-io/date-fns";
+import React, { Children } from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { DatePicker as MuiDatePicker } from "@mui/x-date-pickers/DatePicker";
 
 const DatePicker = (props) => {
   const { name, label, value, onChange } = props;
+
+  const convertToDefaultPara = (name, value) => ({
+    target : {
+      name, value
+    }
+  })
   return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <KeyboardDatePicker
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <MuiDatePicker
+        dateAdapter={AdapterDateFns}
         disableToolbar
         variant="inline"
         inputVariant="outlined"
         formate="MMM/dd/yyy"
         name={name}
         value={value}
-        onChange={onChange}
-      ></KeyboardDatePicker>
-    </MuiPickersUtilsProvider>
+        onChange={date => onChange(convertToDefaultPara(name, date))}
+      />
+    </LocalizationProvider>
   );
 };
 
